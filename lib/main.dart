de157e753da71825fd0bfb8d82dadcc2b7c96ff7
Moustajil Aimad <moustajil.dev@gmail.com>
2024-12-14 13:22:42 +0100

    buils stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,36 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.purpleAccent,
-                Colors.purple,
-                Color.fromARGB(255, 103, 37, 158),
-                Colors.deepPurpleAccent
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello Word",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-      ),
+      home: Scaffold(backgroundColor: Colors.red, body: GradiantContainer()),
     ),
   );
+}
+
+class GradiantContainer extends StatelessWidget {
+  GradiantContainer(super.key){
+
+  }
+  @override
+  Widget build(constext) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.purpleAccent,
+            Colors.purple,
+            Color.fromARGB(255, 103, 37, 158),
+            Colors.deepPurpleAccent
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello Word",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
 }
